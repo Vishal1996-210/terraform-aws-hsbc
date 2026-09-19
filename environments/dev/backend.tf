77f7c76abf57c47189cea2bbd0962a5a032c29f6
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 16:45:45 +0530

    Update S3 backend configuration for production

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,13 @@
 terraform {
   backend "s3" {
-    bucket       = "YOUR-STATE-BUCKET-NAME"
-    key          = "hsbc/dev/terraform.tfstate"
-    region       = "ap-south-1"
+    bucket = "YOUR-STATE-BUCKET-NAME"
+
+    key = "hsbc/prod/terraform.tfstate"
+
+    region = "ap-south-1"
+
     use_lockfile = true
-    encrypt      = true
+
+    encrypt = true
   }
 }
