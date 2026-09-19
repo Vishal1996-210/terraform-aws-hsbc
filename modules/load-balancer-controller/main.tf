1ea47a24feb626fcef06e3f603213b61b169b705
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 20:35:03 +0530

    Update main.tf

--- modules/load-balancer-controller/main.tf
+++ modules/load-balancer-controller/main.tf
@@ -36,6 +36,41 @@ resource "aws_iam_role" "this" {
   }
 }
 
+resource "helm_release" "this" {
+  name       = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "1.14.0"
+
+  set = [
+    {
+      name  = "clusterName"
+      value = var.cluster_name
+    },
+    {
+      name  = "region"
+      value = var.aws_region
+    },
+    {
+      name  = "vpcId"
+      value = var.vpc_id
+    },
+    {
+      name  = "serviceAccount.create"
+      value = "true"
+    },
+    {
+      name  = "serviceAccount.name"
+      value = "aws-load-balancer-controller"
+    }
+  ]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.this
+  ]
+}
+
 resource "aws_iam_role_policy_attachment" "this" {
   role       = aws_iam_role.this.name
   policy_arn = aws_iam_policy.this.arn
