b41effda0fdd63a9e2c4bb9684026a98fd9a9377
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 15:52:55 +0530

    Create variables.tf

---
+++ bootstrap/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "state_bucket_name" {
+  description = "S3 bucket used for Terraform state"
+  type        = string
+}
