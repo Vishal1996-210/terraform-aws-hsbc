2b300b3b3a3b9c64f882a6819b26ca054e901b0b
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 16:51:39 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -17,39 +17,42 @@ resource "aws_internet_gateway" "this" {
 }
 
 resource "aws_subnet" "public" {
-  count = length(var.public_subnet_cidrs)
+  count = length(var.availability_zones)
 
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.public_subnet_cidrs[count.index]
   availability_zone       = var.availability_zones[count.index]
+  cidr_block              = var.public_subnet_cidrs[count.index]
   map_public_ip_on_launch = true
 
   tags = {
     Name = "${var.project_name}-${var.environment}-public-${count.index + 1}"
+    Tier = "public"
   }
 }
 
-resource "aws_subnet" "private_app" {
-  count = length(var.private_app_subnet_cidrs)
+resource "aws_subnet" "private_eks" {
+  count = length(var.availability_zones)
 
   vpc_id            = aws_vpc.this.id
-  cidr_block        = var.private_app_subnet_cidrs[count.index]
   availability_zone = var.availability_zones[count.index]
+  cidr_block        = var.private_eks_subnet_cidrs[count.index]
 
   tags = {
-    Name = "${var.project_name}-${var.environment}-private-app-${count.index + 1}"
+    Name = "${var.project_name}-${var.environment}-private-eks-${count.index + 1}"
+    Tier = "private-eks"
   }
 }
 
 resource "aws_subnet" "private_db" {
-  count = length(var.private_db_subnet_cidrs)
+  count = length(var.availability_zones)
 
   vpc_id            = aws_vpc.this.id
-  cidr_block        = var.private_db_subnet_cidrs[count.index]
   availability_zone = var.availability_zones[count.index]
+  cidr_block        = var.private_db_subnet_cidrs[count.index]
 
   tags = {
     Name = "${var.project_name}-${var.environment}-private-db-${count.index + 1}"
+    Tier = "private-db"
   }
 }
 
@@ -74,44 +77,49 @@ resource "aws_route_table_association" "public" {
 }
 
 resource "aws_eip" "nat" {
+  count  = length(var.availability_zones)
   domain = "vpc"
 
   tags = {
-    Name = "${var.project_name}-${var.environment}-nat-eip"
+    Name = "${var.project_name}-${var.environment}-nat-eip-${count.index + 1}"
   }
 }
 
 resource "aws_nat_gateway" "this" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id
+  count = length(var.availability_zones)
+
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
 
   depends_on = [
     aws_internet_gateway.this
   ]
 
   tags = {
-    Name = "${var.project_name}-${var.environment}-nat"
+    Name = "${var.project_name}-${var.environment}-nat-${count.index + 1}"
   }
 }
 
-resource "aws_route_table" "private_app" {
+resource "aws_route_table" "private_eks" {
+  count = length(var.availability_zones)
+
   vpc_id = aws_vpc.this.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.this.id
+    nat_gateway_id = aws_nat_gateway.this[count.index].id
   }
 
   tags = {
-    Name = "${var.project_name}-${var.environment}-private-app-rt"
+    Name = "${var.project_name}-${var.environment}-private-eks-rt-${count.index + 1}"
   }
 }
 
-resource "aws_route_table_association" "private_app" {
-  count = length(aws_subnet.private_app)
+resource "aws_route_table_association" "private_eks" {
+  count = length(aws_subnet.private_eks)
 
-  subnet_id      = aws_subnet.private_app[count.index].id
-  route_table_id = aws_route_table.private_app.id
+  subnet_id      = aws_subnet.private_eks[count.index].id
+  route_table_id = aws_route_table.private_eks[count.index].id
 }
 
 resource "aws_route_table" "private_db" {
