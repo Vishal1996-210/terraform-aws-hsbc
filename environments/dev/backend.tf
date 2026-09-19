131f9d3c7c5c8b9fcf88d31cb6955778ef3ad28f
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 15:56:02 +0530

    Add Terraform S3 backend configuration

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    bucket       = "YOUR-STATE-BUCKET-NAME"
+    key          = "hsbc/dev/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
