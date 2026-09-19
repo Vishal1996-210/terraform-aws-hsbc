dbd3d7e542e3b34d0fef96943d8dd9a05a93576d
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 20:30:38 +0530

    Add Helm and Kubernetes providers to Terraform config

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -6,6 +6,16 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.38"
+    }
   }
 }
 
@@ -19,4 +29,30 @@ provider "aws" {
       ManagedBy   = "Terraform"
     }
   }
+}
+
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+
+  cluster_ca_certificate = base64decode(
+    data.aws_eks_cluster.this.certificate_authority[0].data
+  )
+
+  token = data.aws_eks_cluster_auth.this.token
 }
