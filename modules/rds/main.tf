1c7cfbc25da2d85d7c4f31497eb71fc15463cc3e
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-20 00:28:17 +0530

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -10,7 +10,7 @@ resource "aws_db_subnet_group" "this" {
 
 resource "aws_db_parameter_group" "mysql" {
   name        = "${var.project_name}-${var.environment}-mysql-parameter-group"
-  family      = "mysql8.0"
+  family      = "mysql8.4"
   description = "MySQL parameter group for ${var.project_name}-${var.environment}"
 
   tags = {
