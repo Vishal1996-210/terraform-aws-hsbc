4d7c2afb4a93fc59820969caf4bf72c9f232d329
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 19:39:36 +0530

    Add outputs for EKS cluster and node groups

--- environments/dev/output.tf
+++ environments/dev/output.tf
@@ -47,3 +47,28 @@ output "ecr_repository_urls" {
   description = "Production ECR repository URLs"
   value       = module.ecr.repository_urls
 }
+
+output "eks_cluster_name" {
+  description = "Production EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
+output "eks_cluster_arn" {
+  description = "Production EKS cluster ARN"
+  value       = module.eks.cluster_arn
+}
+
+output "eks_cluster_endpoint" {
+  description = "Production EKS API endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "eks_system_node_group" {
+  description = "Production EKS system node group"
+  value       = module.eks.system_node_group_name
+}
+
+output "eks_application_node_group" {
+  description = "Production EKS application node group"
+  value       = module.eks.application_node_group_name
+}
