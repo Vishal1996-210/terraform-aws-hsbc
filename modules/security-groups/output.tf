ba4139f95b4e2d016b95832c8e3747cfc20c1162
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 19:57:09 +0530

    Update output.tf

--- modules/security-groups/output.tf
+++ modules/security-groups/output.tf
@@ -17,3 +17,8 @@ output "eks_cluster_security_group_id" {
   description = "EKS cluster security group ID"
   value       = aws_security_group.eks_cluster.id
 }
+
+output "eks_node_security_group_id" {
+  description = "Security group ID for EKS worker nodes"
+  value       = aws_security_group.eks_node.id
+}
