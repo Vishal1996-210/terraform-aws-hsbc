cea194c29cbfe24ae9063f245c125d04978cd54a
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 16:53:27 +0530

    Update output.tf

--- environments/dev/output.tf
+++ environments/dev/output.tf
@@ -1,46 +1,19 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  description = "Production VPC ID"
+  value       = module.vpc.vpc_id
 }
 
 output "public_subnet_ids" {
-  value = module.vpc.public_subnet_ids
+  description = "Production public subnet IDs"
+  value       = module.vpc.public_subnet_ids
 }
 
-output "private_app_subnet_ids" {
-  value = module.vpc.private_app_subnet_ids
+output "private_eks_subnet_ids" {
+  description = "Production EKS subnet IDs"
+  value       = module.vpc.private_eks_subnet_ids
 }
 
 output "private_db_subnet_ids" {
-  value = module.vpc.private_db_subnet_ids
-}
-
-output "alb_dns_name" {
-  description = "Dev ALB DNS name"
-  value       = module.alb.alb_dns_name
-}
-
-output "autoscaling_group_name" {
-  description = "Application Auto Scaling Group"
-  value       = module.autoscaling.autoscaling_group_name
-}
-
-output "launch_template_id" {
-  description = "Application Launch Template"
-  value       = module.autoscaling.launch_template_id
-}
-
-output "rds_endpoint" {
-  value = module.rds.db_endpoint
-}
-
-output "rds_port" {
-  value = module.rds.db_port
-}
-
-output "rds_instance_id" {
-  value = module.rds.db_instance_id
-}
-
-output "rds_secret_arn" {
-  value = module.rds.master_user_secret_arn
+  description = "Production DB subnet IDs"
+  value       = module.vpc.private_db_subnet_ids
 }
