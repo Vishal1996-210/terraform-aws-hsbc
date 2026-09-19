52e92b1162de9f6d8891d6a0bc4265f0fee52dde
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 14:29:16 +0530

    Add VPC module configuration for development environment

---
+++ environments/dev/main.tf
@@ -0,0 +1,28 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  vpc_cidr = "10.10.0.0/16"
+
+  availability_zones = [
+    "ap-south-1a",
+    "ap-south-1b"
+  ]
+
+  public_subnet_cidrs = [
+    "10.10.1.0/24",
+    "10.10.2.0/24"
+  ]
+
+  private_app_subnet_cidrs = [
+    "10.10.11.0/24",
+    "10.10.12.0/24"
+  ]
+
+  private_db_subnet_cidrs = [
+    "10.10.21.0/24",
+    "10.10.22.0/24"
+  ]
+}
