7abb3d022632f8c87c6bef2dbcb1b5ce74d4cba8
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 15:45:52 +0530

    Add variables for security group module

---
+++ modules/security-groups/variables.tf
@@ -0,0 +1,20 @@
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "allowed_ssh_cidr" {
+  description = "CIDR allowed for SSH access to application servers"
+  type        = string
+  default     = null
+}
