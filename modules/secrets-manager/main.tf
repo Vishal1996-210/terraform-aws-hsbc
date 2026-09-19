70d6eb5fc87a5c1d561a4e7a9e3e7adf6d9d758f
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 20:22:08 +0530

    Update main.tf

--- modules/secrets-manager/main.tf
+++ modules/secrets-manager/main.tf
@@ -1,9 +1,52 @@
-resource "aws_secretsmanager_secret" "rds" {
-  name = var.secret_name
+resource "aws_iam_role" "rds_secret_reader" {
+  name = "${var.project_name}-${var.environment}-rds-secret-reader"
 
-  kms_key_id = var.kms_key_arn
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+      }
+    ]
+  })
 
   tags = {
-    Name = var.secret_name
+    Name = "${var.project_name}-${var.environment}-rds-secret-reader"
   }
+}
+
+resource "aws_iam_policy" "rds_secret_reader" {
+  name = "${var.project_name}-${var.environment}-rds-secret-reader"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret"
+        ]
+
+        Resource = var.rds_secret_arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "rds_secret_reader" {
+  role       = aws_iam_role.rds_secret_reader.name
+  policy_arn = aws_iam_policy.rds_secret_reader.arn
 }
