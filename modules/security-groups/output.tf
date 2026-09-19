d092984cc172e6cfc59b8bdd746c8a3ce179b689
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 23:39:13 +0530

    Update output.tf

--- modules/security-groups/output.tf
+++ modules/security-groups/output.tf
@@ -3,11 +3,6 @@ output "alb_security_group_id" {
   value       = aws_security_group.alb.id
 }
 
-output "eks_application_security_group_id" {
-  description = "Security group ID for EKS application workloads"
-  value       = aws_security_group.eks_application.id
-}
-
 output "rds_security_group_id" {
   description = "Security group ID for RDS"
   value       = aws_security_group.rds.id
