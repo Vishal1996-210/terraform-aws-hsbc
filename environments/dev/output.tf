55ac289c6b7932e0532bd2fbae01a76e9457a68b
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 14:50:17 +0530

    Add outputs for autoscaling group and launch template

--- environments/dev/output.tf
+++ environments/dev/output.tf
@@ -18,3 +18,13 @@ output "alb_dns_name" {
   description = "Dev ALB DNS name"
   value       = module.alb.alb_dns_name
 }
+
+output "autoscaling_group_name" {
+  description = "Application Auto Scaling Group"
+  value       = module.autoscaling.autoscaling_group_name
+}
+
+output "launch_template_id" {
+  description = "Application Launch Template"
+  value       = module.autoscaling.launch_template_id
+}
