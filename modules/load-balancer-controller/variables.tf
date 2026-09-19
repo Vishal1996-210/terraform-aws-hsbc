9469ba9efa530006fc04e3e78afdc9183d49ee99
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 20:32:13 +0530

    Update variables.tf

--- modules/load-balancer-controller/variables.tf
+++ modules/load-balancer-controller/variables.tf
@@ -12,3 +12,18 @@ variable "cluster_name" {
   description = "EKS cluster name"
   type        = string
 }
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "iam_role_arn" {
+  description = "IAM role ARN used by AWS Load Balancer Controller"
+  type        = string
+}
