52fbc16c285399db8c6b870921ee0f549ee3bb8c
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 16:52:44 +0530

    Add VPC and subnet variables for production
    Added variables for VPC CIDR, availability zones, and subnet CIDRs.

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -12,3 +12,28 @@ variable "project_name" {
   description = "Project name"
   type        = string
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the production VPC"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "Availability Zones for the production VPC"
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_eks_subnet_cidrs" {
+  description = "CIDR blocks for private EKS subnets"
+  type        = list(string)
+}
+
+variable "private_db_subnet_cidrs" {
+  description = "CIDR blocks for private DB subnets"
+  type        = list(string)
+}
