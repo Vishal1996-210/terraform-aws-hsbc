d1c960609d266ea49257668f38aff8fe1095309b
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 16:14:07 +0530

    Configure RDS module for MySQL database
    Added RDS module configuration for MySQL database.

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -83,3 +83,30 @@ module "iam" {
   project_name = var.project_name
   environment  = var.environment
 }
+
+module "rds" {
+  source = "../../modules/rds"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  private_db_subnet_ids = module.vpc.private_db_subnet_ids
+
+  security_group_id = module.security_groups.db_security_group_id
+
+  engine         = "mysql"
+  engine_version = "8.0"
+
+  instance_class = "db.t3.micro"
+
+  allocated_storage     = 20
+  max_allocated_storage = 50
+
+  database_name = "hsbcbank"
+
+  master_username = "admin"
+
+  multi_az = false
+
+  backup_retention_period = 7
+}
