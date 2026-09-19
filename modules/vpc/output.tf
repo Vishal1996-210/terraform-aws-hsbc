0fd8bdd1eb4bed7485f02a70952337980b0d8313
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 16:51:58 +0530

    Update output.tf

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,15 +1,29 @@
 output "vpc_id" {
-  value = aws_vpc.this.id
+  description = "ID of the VPC"
+  value       = aws_vpc.this.id
+}
+
+output "vpc_cidr" {
+  description = "CIDR block of the VPC"
+  value       = aws_vpc.this.cidr_block
 }
 
 output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
+  description = "IDs of public subnets"
+  value       = aws_subnet.public[*].id
 }
 
-output "private_app_subnet_ids" {
-  value = aws_subnet.private_app[*].id
+output "private_eks_subnet_ids" {
+  description = "IDs of private EKS subnets"
+  value       = aws_subnet.private_eks[*].id
 }
 
 output "private_db_subnet_ids" {
-  value = aws_subnet.private_db[*].id
+  description = "IDs of private DB subnets"
+  value       = aws_subnet.private_db[*].id
+}
+
+output "nat_gateway_ids" {
+  description = "IDs of NAT Gateways"
+  value       = aws_nat_gateway.this[*].id
 }
