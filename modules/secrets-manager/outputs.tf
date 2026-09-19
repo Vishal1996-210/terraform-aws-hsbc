81fb881173caab48b9df704a3524d62e673b78c8
Vishal1996-210 <99161104+Vishal1996-210@users.noreply.github.com>
2026-09-19 20:02:17 +0530

    Create outputs.tf

---
+++ modules/secrets-manager/outputs.tf
@@ -0,0 +1,9 @@
+output "secret_arn" {
+  description = "ARN of the RDS secret"
+  value       = aws_secretsmanager_secret.rds.arn
+}
+
+output "secret_name" {
+  description = "Name of the RDS secret"
+  value       = aws_secretsmanager_secret.rds.name
+}
